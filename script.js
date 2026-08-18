eb1aa232b4aca04008cd41309aa0630987dd9bed
Panquesocremón LRM <panquesocremon142@gmail.com>
2026-08-18 19:33:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("keydown", function (event) {
+    if (
+        event.key === "F12" ||
+        (event.ctrlKey && event.shiftKey && event.key === "I") ||
+        (event.ctrlKey && event.shiftKey && event.key === "J") ||
+        (event.ctrlKey && event.shiftKey && event.key === "C")
+    ) {
+        event.preventDefault();
+    }
+});
